761d4e34049ed6bb8d15ccbc96feed1006e4ac34
Thong <thongnguyen1292@gmail.com>
2017-07-01 12:28:07 +0200

    use python 2.7

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
 export PYTHONPATH=$PYTHONPATH:$PWD/src/python
 export PATH=$PWD/bin:$PATH
+alias python=/cvmfs/cms.cern.ch/slc6_amd64_gcc530/cms/cmssw/CMSSW_9_0_0/external/slc6_amd64_gcc530/bin/python
