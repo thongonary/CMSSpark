3f5871402d03fe6d71cad4230516509fed180e42
Thong <thongnguyen1292@gmail.com>
2018-07-18 20:51:37 +0200

    sample code to get all data

---
+++ getwma_all.sh
@@ -0,0 +1,18 @@
+#run_spark wmarchive.py --fout=hdfs:///cms/users/qnguyen/wma20170601 --date=20170601-20170610 --yarn
+#run_spark wmarchive.py --fout=hdfs:///cms/users/qnguyen/wma20170602 --date=20170611-20170620 --yarn
+#run_spark wmarchive.py --fout=hdfs:///cms/users/qnguyen/wma20170603 --date=20170621-20170631 --yarn
+#run_spark wmarchive.py --fout=hdfs:///cms/users/qnguyen/wma20170501 --date=20170501-20170510 --yarn
+#run_spark wmarchive.py --fout=hdfs:///cms/users/qnguyen/wma20170502 --date=20170511-20170520 --yarn
+#run_spark wmarchive.py --fout=hdfs:///cms/users/qnguyen/wma20170503 --date=20170521-20170531 --yarn
+#run_spark wmarchive.py --fout=hdfs:///cms/users/qnguyen/wma20170401 --date=20170401-20170410 --yarn
+#run_spark wmarchive.py --fout=hdfs:///cms/users/qnguyen/wma20170402 --date=20170411-20170420 --yarn
+#run_spark wmarchive.py --fout=hdfs:///cms/users/qnguyen/wma20170403 --date=20170421-20170431 --yarn
+#run_spark wmarchive.py --fout=hdfs:///cms/users/qnguyen/wma20170301 --date=20170301-20170310 --yarn
+#run_spark wmarchive.py --fout=hdfs:///cms/users/qnguyen/wma20170302 --date=20170311-20170320 --yarn
+#run_spark wmarchive.py --fout=hdfs:///cms/users/qnguyen/wma20170303 --date=20170321-20170331 --yarn
+#run_spark wmarchive.py --fout=hdfs:///cms/users/qnguyen/wma20170201 --date=20170201-20170210 --yarn
+#run_spark wmarchive.py --fout=hdfs:///cms/users/qnguyen/wma20170202 --date=20170211-20170220 --yarn
+#run_spark wmarchive.py --fout=hdfs:///cms/users/qnguyen/wma20170203 --date=20170221-20170231 --yarn
+#run_spark wmarchive.py --fout=hdfs:///cms/users/qnguyen/wma20170101 --date=20170101-20170110 --yarn
+#run_spark wmarchive.py --fout=hdfs:///cms/users/qnguyen/wma20170102 --date=20170111-20170120 --yarn
+run_spark wmarchive.py --fout=hdfs:///cms/users/qnguyen/test0 --date=20170121-20170131 
