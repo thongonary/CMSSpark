31f9da8e6f002afa34656432a115cb34608f5e2a
Thong <thongnguyen1292@gmail.com>
2018-07-18 20:27:18 +0200

    Use local cmssw

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 export PYTHONPATH=$PYTHONPATH:$PWD/src/python
 export PATH=$PWD/bin:$PATH
 
-cd /afs/cern.ch/work/q/qnguyen/public/PulseShape/CMSSW_9_0_0/src
+cd CMSSW_9_2_0/src
 eval `scramv1 runtime -sh`
 cd -
